2c580549021cfed068869d2041ec292ee969ef36
Boyuzhou <2470200492@qq.com>
2018-01-30 23:55:05 +0800

    fix: demo

--- demo/demolist/Demo2.js
+++ demo/demolist/Demo2.js
@@ -1,26 +1,24 @@
 /**
  *
- * @title 默认的模态框
+ * @title 注册的模态框
  * @description
  *
  */
 
-import { Con, Row, Col } from 'bee-layout';
-import { Panel } from 'bee-panel';
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Button from 'bee-button';
 import Modal from '../../src';
-import Form from 'bee-form';
 import FormGroup from 'bee-form-group';
-import ControlLabel from 'bee-control-label';
+import Label from 'bee-label';
 import FormControl from 'bee-form-control';
-const FormItem = Form.FormItem;
 
 class Demo2 extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            showModal: false
+            showModal: false,
+            password: '',
+            name: ''
         };
         this.close = this.close.bind(this);
         this.open = this.open.bind(this);
@@ -38,69 +36,63 @@ class Demo2 extends Component {
         });
     }
 
-    checkForm = (flag,obj) => {
-        if(flag){
-            this.close();
-        }
+    handleChange = (state) => (value) => {
+        this.setState({
+            [state]: value
+        })
     }
 
     render() {
 
-        const CancelBtn = (
-            <Button style={{ marginRight: 20 }} onClick={ this.close }>取消</Button>
-        )
-
         return (
-        <div className="demo-margin">
-            <Button
-            colors = "primary"
-            size = "lg"
-            onClick = { this.open }>
-                注册模态框
-            </Button>​
-            <Modal
-            show = { this.state.showModal }
-            onHide = { this.close }
-            style={{ width: 450}}
-            >
-                <Modal.Header className="text-center">
-                    <Modal.Title>注册</Modal.Title>
-                </Modal.Header>
+            <div className="demo-margin">
+                <Button
+                    colors="primary"
+                    size="lg"
+                    onClick={this.open}>
+                    注册模态框
+                </Button>​
+                <Modal
+                    show={this.state.showModal}
+                    onHide={this.close}
+                    style={{width: 450}}
+                >
+                    <Modal.Header className="text-center">
+                        <Modal.Title>注册</Modal.Title>
+                    </Modal.Header>
 
-                <Modal.Body>
+                    <Modal.Body>
+                        <div style={{ width: 300, margin: '0 auto' }}>
+                            <FormGroup>
+                                <Label>用户名</Label>
+                                <FormControl
+                                    value={this.state.name}
+                                    onChange={this.handleChange('name')}
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label>密码</Label>
+                                <FormControl
+                                    value={this.state.password}
+                                    onChange={this.handleChange('password')}
+                                />
+                            </FormGroup>
+                        </div>
 
-                    <Form
-                        submitCallBack={this.checkForm}
-                        submitAreaClassName="text-center"
-                        className="text-center"
-                        beforeSubmitBtn={CancelBtn}>
-                        <FormItem
-                            labelName="姓名: "
-                            isRequire={true}
-                            htmlType="chinese"
-                            errorMessage="姓名格式错误"
-                            method="blur"
-                            inline={true}>
-                            <FormControl name="name" placeholder="只能输入中文"/>
-                        </FormItem>
-                        <FormItem
-                            labelName="年龄: "
-                            isRequire={true}
-                            method="blur"
-                            errorMessage="年龄格式错误"
-                            reg={/^[0-9]+$/}
-                            inline={true}>
-                            <FormControl name="age" ref="input" />
-                        </FormItem>
-                    </Form>
 
-                </Modal.Body>
+                    </Modal.Body>
 
-                <Modal.Footer>
-                </Modal.Footer>
-           </Modal>
+                    <Modal.Footer className="text-center">
+                        <Button bordered style={{ marginRight: 20 }} onClick={this.close}>
+                            取消
+                        </Button>
+                        <Button colors="primary" onClick={this.close}>
+                            确认
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
 
-        </div>
+            </div>
         )
     }
 }
